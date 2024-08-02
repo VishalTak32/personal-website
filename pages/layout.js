dfc965f20b49d2fceef2e9cc5f70d8e81ae7a942
Vishal Tak <vishal.tak14@ggmail.com>
2024-08-02 13:02:02 -0500

    about page looking clean!

--- pages/layout.js
+++ pages/layout.js
@@ -1,13 +1,31 @@
 import { AppBar, Toolbar, Typography, Container, Link as MuiLink, Box } from '@mui/material';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export default function Layout({ children }) {
+  const router = useRouter();
+
+  const handleAboutClick = (e) => {
+    e.preventDefault();
+    if (router.pathname !== '/') {
+      router.push('/');
+    } else {
+      const aboutSection = document.getElementById('about-section');
+      if (aboutSection) {
+        aboutSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  };
   return (
     <div>
       <AppBar position="fixed" sx={{ backgroundImage:'none', boxShadow:'none', width: '100%', top: 0, left: 0 }}>
-        <Toolbar sx={{ justifyContent: 'right' }}>
+        <Toolbar sx={{ justifyContent: 'right', 
+      '@media (max-width: 600px)': {
+        justifyContent: 'center'
+      },}}>
           <Box sx={{ display: 'flex', gap: 5 }}>
             <Link href="/" passHref>
-              <MuiLink color="inherit" sx={{textDecoration: 'none', margin: '0 10px' }}>
+              <MuiLink color="inherit" sx={{textDecoration: 'none', margin: '0 10px' }} onClick={handleAboutClick}>
                 About
               </MuiLink>
             </Link>
